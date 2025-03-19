2a19a21ef739d3959a0d4df9cee1da7541345d72
Andreas-Lukito <andrews.lukito@gmail.com>
2025-03-19 18:07:07 +0700

    fix: fixed upload file feature

--- Time_series_model.py
+++ Time_series_model.py
@@ -50,7 +50,7 @@ def make_forecast(df, store, product, forecast):
 
 #main function
 def main():
-    df = st.file_uploader(type="csv")
+    df = st.file_uploader('Upload your csv file', type="csv", accept_multiple_files=False)
     store = st.slider('store', min_value=0, max_value=6, value=1)
     product = st.slider('product', min_value=0, max_value=9, value=1)
     forecast_days = st.slider("forecast_days", min_value=0, max_value=365, value=1)
